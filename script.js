f5ad271882b29319b700f583dc7af96ac883d8b2
Bharati Garg <bharatigargkls1999@gmail.com>
2024-06-23 09:34:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+//Navbar Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+};
+//nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
+
+// brand
+var swiper = new Swiper(".our-brand", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  },
+});
+
+//Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3500,
+  },
+});
